395d97b0ea0861ebcdfa49f47fd27a01ad4f31ab
Antoine GROLIER <agrolier@hellowork.com>
2022-12-06 09:20:11 +0100

    ✨ Day 6 - ☝️ in the 👃

---
+++ days/06/day-2.js
@@ -0,0 +1,15 @@
+let resultPosition = 0;
+const sequenceLength = 14;
+
+module.exports = async (readInput, log) => {
+  await readInput((line, eof) => {
+    if(eof) return;
+    for(let i = 0; i < line.length; i++){
+      if(new Set(line.slice(i,i+sequenceLength)).size == sequenceLength){
+        resultPosition = i+sequenceLength;
+        return;
+      }
+    }
+  });
+  return resultPosition;
+};
